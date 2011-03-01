5e1ffbbc9e4823c13a320433801274bedf26d450
Fernando Almeida <fernando@fernandoalmeida.net>
2011-03-01 02:50:19 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FastTicket::Application.config.secret_token = '9d52dc31f773e01c4b797429847803319bb2058f844e0be797c701796cad9eb649617a0b4b8d762b8f1d5b61fa16fe031554d506621f93c59ed4b4c88eba2e87'
